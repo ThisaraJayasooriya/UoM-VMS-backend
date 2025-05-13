5ea99946cb81d1bf3700b092ae970380c95277a3
Thisara-Jayasooriya <jthisaradj2001@gmail.com>
2025-05-13 14:57:32 +0530

    updated backend of requesting a meeting

--- routes/appoiment.routes.js
+++ routes/appoiment.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { makeAppoinment } from '../controllers/appoinment.controller.js';
+import { getAllHosts } from '../controllers/appoinment.controller.js';
 
 const router = express.Router();
 
 router.post('/createAppointment', makeAppoinment);
+router.get('/gethosts', getAllHosts);
 
 export default router;
