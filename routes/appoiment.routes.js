1ebdb30dc59736a2b8f2215ac7addc17556f8731
unknown <dulanganikeshala2@gmail.com>
2025-05-18 15:01:06 +0530

    updated host available time slots page

--- routes/appoiment.routes.js
+++ routes/appoiment.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { makeAppoinment } from '../controllers/appoinment.controller.js';
 import { getAllHosts } from '../controllers/appoinment.controller.js';
+import { getAcceptedAppointment } from '../controllers/appoinment.controller.js';
+
+
+
 
 const router = express.Router();
 
 router.post('/createAppointment', makeAppoinment);
 router.get('/gethosts', getAllHosts);
-
+router.get("/acceptedAppointment/:visitorId", getAcceptedAppointment);
 export default router;
