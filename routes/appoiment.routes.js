d0277ae7bc600510bfcd69330646e6ba0d813221
unknown <dulanganikeshala2@gmail.com>
2025-07-14 21:43:43 +0530

    updated visitor Edit Profile page and Update Host selection

--- routes/appoiment.routes.js
+++ routes/appoiment.routes.js
@@ -2,6 +2,9 @@ import express from 'express';
 import {
   makeAppoinment,
   getAllHosts,
+  getAllFaculties,
+  getDepartmentsByFaculty,
+  getHostsByFacultyAndDepartment,
   getAcceptedAppointment,
   confirmAppointment,
   visitorRejectAppointment,
@@ -13,6 +16,9 @@ const router = express.Router();
 
 router.post('/createAppointment', makeAppoinment);
 router.get('/gethosts', getAllHosts);
+router.get('/faculties', getAllFaculties);
+router.get('/departments/:faculty', getDepartmentsByFaculty);
+router.get('/hosts/:faculty/:department', getHostsByFacultyAndDepartment);
 router.get("/acceptedAppointment/:visitorId", getAcceptedAppointment);
 router.put("/confirmAppointment/:appointmentId", confirmAppointment);
 router.put("/rejectAppointments/:appointmentId", visitorRejectAppointment); 
--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProfileData } from "../controllers/userProfileController.js";
+import { getProfileData, updateProfileData } from "../controllers/userProfileController.js";
 
 const router = express.Router();
 
 router.get("/:userId", getProfileData);
+router.put("/:userId", updateProfileData);
 
 export default router;
