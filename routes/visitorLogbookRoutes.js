3ac5cf62f4b24690a615e64751e6d9d9bed44d4e
Hafsa-M1 <mushtaqfh.22@uom.lk>
2025-07-27 14:56:21 +0530

    Updated logbook controller and routes after removing delete

--- routes/visitorLogbookRoutes.js
+++ routes/visitorLogbookRoutes.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { getVisitorLogbook, deleteLogEntry } from '../controllers/visitorLogbookController.js';
-import { verifyToken } from '../controllers/AuthController.js';
+import { getVisitorLogbook } from '../controllers/visitorLogbookController.js';
 
 const router = express.Router();
 
 // GET all logbook entries
 router.get('/', getVisitorLogbook);
-
-// DELETE to remove a specific logbook entry by visitorId
-router.delete('/:id', verifyToken, deleteLogEntry);
 
 export default router;
